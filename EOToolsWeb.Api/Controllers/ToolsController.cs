092a82b20a3d4e72189cecb7d7b27860f370faf0
Jebzou <22751386+Jebzou@users.noreply.github.com>
2024-07-22 18:38:03 +0200

    Updater route

---
+++ EOToolsWeb.Api/Controllers/ToolsController.cs
@@ -0,0 +1,24 @@
+﻿using EOToolsWeb.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EOToolsWeb.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ToolsController(ConfigurationService config) : ControllerBase
+{
+    private ConfigurationService ConfigurationService { get; } = config;
+
+    [HttpGet("GetUpdate")]
+    public async Task<IActionResult> GetUpdate(string userVersion)
+    {
+        string latestVersionString = await ConfigurationService.GetSoftwareVersion();
+
+        if (latestVersionString != userVersion)
+        {
+            return File(System.IO.File.OpenRead(Path.Combine("Assets", "EOTools.zip")), "application/zip");
+        }
+
+        return Ok();
+    }
+}
--- EOToolsWeb.Api/Services/ConfigurationService.cs
+++ EOToolsWeb.Api/Services/ConfigurationService.cs
@@ -4,12 +4,19 @@ namespace EOToolsWeb.Api.Services;
 
 public class ConfigurationService
 {
-    public string DataRepoUrl { get; set; } = "";
-
+    public string DataRepoUrl { get; private set; } = "";
+    
     public async Task Initialize()
     {
-        Dictionary<string, string>? config = JsonSerializer.Deserialize<Dictionary<string, string>>(await File.ReadAllTextAsync("Config.json"));
+        Dictionary<string, string>? config = JsonSerializer.Deserialize<Dictionary<string, string>>(await File.ReadAllTextAsync(Path.Combine("Assets", "Config.json")));
 
         DataRepoUrl = config?["dataRepoUrl"] ?? "";
     }
+
+    public async Task<string> GetSoftwareVersion()
+    {
+        Dictionary<string, string>? config = JsonSerializer.Deserialize<Dictionary<string, string>>(await File.ReadAllTextAsync(Path.Combine("Assets", "Config.json")));
+
+        return config?["softwareVersion"] ?? "0.0.0.0";
+    } 
 }
