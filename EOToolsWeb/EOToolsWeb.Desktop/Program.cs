9ad0e0de117afec2d0bfb0616f8a7327643c6fb3
Jebzou <22751386+Jebzou@users.noreply.github.com>
2025-11-14 22:41:57 +0100

    Fix

--- EOToolsWeb/EOToolsWeb.Desktop/Program.cs
+++ EOToolsWeb/EOToolsWeb.Desktop/Program.cs
@@ -13,6 +13,10 @@ namespace EOToolsWeb.Desktop
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
 
+        // TODO : Need to replace the nuget Avalonia.ReactiveUI by ReactiveUI.Avalonia
+        // This can't be done for the moment cause of an issue on startup
+        // See https://github.com/reactiveui/ReactiveUI/issues/4123
+
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
